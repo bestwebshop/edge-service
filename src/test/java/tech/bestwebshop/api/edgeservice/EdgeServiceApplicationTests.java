9a072494efe26409baaba0323c88dbedbd6af8ab
Wekra <github@cwernet.de>
2019-12-28 20:17:44 +0100

    Initial commit after moving sources from api repository

---
+++ src/test/java/tech/bestwebshop/api/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bestwebshop.api.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
